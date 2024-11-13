22490b15888af67be2079003ac2124d11f38196c
oliver <oliver.meek2@gmail.com>
2024-11-12 20:58:02 -0500

    added songs and genres endpoints

--- app/main.py
+++ app/main.py
@@ -6,6 +6,7 @@ from pydantic import BaseModel
 import json
 import os
 import mysql.connector
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 DBHOST = "ds2022.cqee4iwdcaph.us-east-1.rds.amazonaws.com"
@@ -13,8 +14,47 @@ DBUSER = "admin"
 DBPASS = os.getenv('DBPASS')
 DB = "azt6gn"
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allow all origins; replace "*" with specific origins if needed
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 db = mysql.connector.connect(user=DBUSER, host=DBHOST, password=DBPASS, database=DB)
 cur=db.cursor()
+
+@app.get("/genres")
+async def get_genres():
+    query = "SELECT * FROM genres ORDER BY genreid;"
+    try:
+        cur.execute(query)
+        headers = [x[0] for x in cur.description]
+        results = cur.fetchall()
+        json_data = [dict(zip(headers, result)) for result in results]
+        return json_data
+    except mysql.connector.Error as e:
+        print("MySQL Error:", str(e))
+        return {"error": str(e)}
+    finally:
+        cur.close()
+
+@app.get("/songs")
+async def get_songs():
+    query = "SELECT songs.title, songs.album, songs.artist, songs.year, songs.file, songs.image, genres.genre FROM songs JOIN genres ON songs.genre = genres.genreid;"  # Complete SQL query here
+    try:
+        cur.execute(query)
+        headers = [x[0] for x in cur.description]
+        results = cur.fetchall()
+        json_data = [dict(zip(headers, result)) for result in results]
+        return json_data
+    except mysql.connector.Error as e:
+        print("MySQL Error:", str(e))
+        return {"error": str(e)}
+    finally:
+        cur.close()
+
 
 @app.get("/")  # zone apex
 def zone_apex():
